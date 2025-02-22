1a2db53d8d456f9771c9fe214af1f50ab391d9e7
Raul Emilio Astete Acuña <raulastete.2215@gmail.com>
2025-02-22 17:25:27 -0500

    Create get all articles endpoint with test

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\ArticleCollection;
 use App\Http\Resources\ArticleResource;
 use App\Models\Article;
-use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
+    public function index() : ArticleCollection
+    {
+        return ArticleCollection::make(Article::all());
+    }
+
     public function show(Article $article)
     {
         return ArticleResource::make($article);
--- routes/api.php
+++ routes/api.php
@@ -3,3 +3,4 @@ use App\Http\Controllers\ArticleController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('articles/{article}', [ArticleController::class, 'show'] )->name('api.v1.articles.show');
+Route::get('articles', [ArticleController::class, 'index'] )->name('api.v1.articles.index');
